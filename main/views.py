6bb9c38bfdbf0ff551f0b1386a31c3fb4fc8dd22
Juan Vertiz <juanpablovertiz@hotmail.com>
2024-08-15 03:35:36 -0600

    add: create base, index and contact (incomplete) templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """View function for Home page"""
+    context = {
+        "skills": {
+            "Python": 4, 
+            "PHP": 3,
+            "JavaScript": 3,
+            "Java": 3,
+            "C/C++": 2,
+            "HTML/CSS": 3,
+            "Docker": 3,
+            "Git": 3,
+            "GitHub": 4
+        },
+        "languages": {
+            "Spanish": 5,
+            "English": 3
+        }
+    }
+    return render(request=request, template_name="main/index.html", context=context)
+
+def contact(request):
+    """View function for Contact page"""
+    
+    return render(request=request, template_name="main/contact.html")
